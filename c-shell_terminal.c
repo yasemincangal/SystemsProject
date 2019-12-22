84fc366bdc4a9bafc5ec577e915010a9cf4d0d1c
yasemincangal <47554994+yasemincangal@users.noreply.github.com>
2019-12-22 21:12:46 +0300

    History shell function has executed in terminal
    history_of_terminal.txt can store current commands too now. Some errors fixed. History function execution is successfull. Linux terminal didn't accept process.h library, then I used system(); . Also, paths and function names will be changed in here and in shell script too, when they are finished.

--- c-shell_terminal.c
+++ c-shell_terminal.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <process.h>
 #include <stdlib.h>
-#define FILE_NAME "history_of_terminal.txt" //save history of terminal as a file
+#define FILE_NAME "/home/yasemin/deneme1/history_of_terminal.txt" //save history of terminal as a file
 
-int main() {
-    FILE *historyfile=fopen(FILE_NAME,"a"); //create a file which stores a history of C-shell terminal
-    char *command=malloc(80*sizeof(char));
+int main(void) {
+char command_[80]; //linux didn't accept pointer even with malloc
+char *command;
+command=command_;
     char function1[]="history"; //function names will be added when the others has finished
     char function2[]="b";
     char function3[]="c";
@@ -19,18 +19,20 @@ int main() {
     printf("zeynepyasemin@SystemsProgramming:~$ ");
     scanf("%s",command);
    // strcat(history,command); //concatenation
-
+//printf("-%s-",command);
+FILE *historyfile=fopen(FILE_NAME,"a");
     if(strcmp(command_name,function1)==0||strcmp(command_name,function2)==0||strcmp(command_name,function3)==0||strcmp(command_name,function4)==0||strcmp(command_name,"exit")==0){
         fprintf(historyfile,"%s\n",command_name); //add that command name into history_of_terminal.txt
     }else{
         printf("That command line is not allowed.\n");
     }
 
-
+fclose(historyfile);
     while(strcmp(command_name,"exit")!=0){ //if user write exit, then finish process
-
+        historyfile=fopen(FILE_NAME,"a"); //create a file which stores a history of C-shell terminal
         if(strcmp(command_name,function1)==0){// if input command is history
-            //execl will be added
+           system("./history.sh /home/yasemin/deneme1/history_of_terminal.txt"); //execution of history shell program
+
         }
 
 
@@ -47,11 +49,12 @@ int main() {
             fprintf(historyfile,"%s\n",command_name);  //add that command name into history_of_terminal.txt
         }else{
             printf("That command line is not allowed.\n");
+            // strcat(history,command); //concatenation
         }
 
-
+fclose(historyfile);
     }
 
-
+ 
     return 0;
 }
